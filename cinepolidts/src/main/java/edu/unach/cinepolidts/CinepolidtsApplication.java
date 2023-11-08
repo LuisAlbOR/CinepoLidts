8ea94bf47bc49e72547a90892b78ce7ac71f4c98
Guicho <luiaballz.laor@gmail.com>
2023-11-08 07:53:51 -0600

    Se crear y sube el proyecto de CinepoLidts, con las depencias básicas que lleva en el pom

---
+++ cinepolidts/src/main/java/edu/unach/cinepolidts/CinepolidtsApplication.java
@@ -0,0 +1,13 @@
+package edu.unach.cinepolidts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinepolidtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinepolidtsApplication.class, args);
+	}
+
+}
---
+++ cinepolidts/src/test/java/edu/unach/cinepolidts/CinepolidtsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unach.cinepolidts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinepolidtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
